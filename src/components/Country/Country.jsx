068a60d973539f026ec4f077e0e0cbfa4427296e
jaky-sheikh <jaky@eu4m.eu>
2025-01-09 22:47:46 +0600

    updating state

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     const { name, flags, population, area, cca3 } = country;
 
     const [visited, setVisited] = useState(false);
@@ -9,13 +9,18 @@ const Country = ({ country }) => {
         setVisited(!visited);
     }
 
+    const passWithParams = () => handleVisitedCountry(country);
+
+
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{ color: visited ? 'tomato' : 'black' }}>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? 'I have visited this country.' : 'I want to visit'}
         </div>
